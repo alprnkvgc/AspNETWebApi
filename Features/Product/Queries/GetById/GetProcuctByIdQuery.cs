0ffefc195ce7f1366be7b56abc6ab9116d553e5d
alprnkvgc <alperen.kavgaci@hotmail.com>
2023-10-18 19:19:58 +0300

    GetProductById query added and updated folder structure

---
+++ Features/Product/Queries/GetById/GetProcuctByIdQuery.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using MediatR;
+
+namespace AspNETWebApi.Features.Product.Queries.GetById
+{
+    public class GetProductByIdQuery : IRequest<GetProductByIdResponse>
+    {
+        public int Id { get; set; }
+    }
+    public class GetProductByIdQueryHandler : IRequestHandler<GetProductByIdQuery, GetProductByIdResponse>
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetProductByIdQueryHandler(ApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public Task<GetProductByIdResponse> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
+        {
+            var product = _context.Products.Where(p => p.Id == request.Id).FirstOrDefault();
+            if (product == null) throw new Exception("Böyle bir ürün bulunmamaktadır");
+            var mappedProduct = _mapper.Map<GetProductByIdResponse>(product);
+            return Task.FromResult(mappedProduct);
+        }
+    }
+}
---
+++ Features/Product/Queries/GetById/GetProductByIdResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace AspNETWebApi.Features.Product.Queries.GetById
+{
+    public class GetProductByIdResponse
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public int Qunatity { get; set; }
+    }
+}
